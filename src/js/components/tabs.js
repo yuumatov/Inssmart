d54f61102d99fb1bc3f095e991504405ec3c77a4
Yuumatov <yuumatov@yandex.ru>
2022-11-03 15:11:50 +0300

    Доработки

--- src/js/components/tabs.js
+++ src/js/components/tabs.js
@@ -2,5 +2,8 @@ import $ from 'jquery';
 
 $('.tabs__control').on('click', function() {
   $('.tabs__control').removeClass('tabs__control--active');
+  $('.tabs__content').removeClass('tabs__content--active');
+  var tabsActiveId = $(this).attr('data-tabs-id');
   $(this).addClass('tabs__control--active');
+  $(`.tabs__content[data-tabs-id="${tabsActiveId}"]`).addClass('tabs__content--active');
 });
